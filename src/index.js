eb4f32ace42de0b6d0798b57458aa116752c975a
CtrlFrontfire <vlachaki.argyro@gmail.com>
2024-06-23 18:57:33 +0100

    Fixed unit issue in HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = temperature;
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let cityUserInput = document.querySelector("#search-input");
+  let city = cityUserInput.value;
 
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = cityUserInput.value;
+  let apiKey = "d32099afo5328e0346f4tb17db5ff833";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 let searchFormElement = document.querySelector("#search-form");
