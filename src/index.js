006934d541cb4b978e4372f0bf8b71fd39fb5ac6
CtrlFrontfire <vlachaki.argyro@gmail.com>
2024-06-23 20:21:09 +0100

    Added api, date and time in JS

--- src/index.js
+++ src/index.js
@@ -19,5 +19,56 @@ function handleSearch(event) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
+function formatDate(date) {
+  let day = date.getDay();
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let dateNow = date.getDate();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let formattedDay = days[day];
+  let formattedMonth = months[month];
+  return `${formattedDay} ${dateNow} ${formattedMonth} ${year}, ${hours}:${minutes}`;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+let currentDateElement = document.querySelector("#weather-app-timestamp");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
